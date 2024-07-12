19487b343c83d3b87e900696b6ed1cbb633e62b8
SergeyZakurakin <pricezak@gmail.com>
2024-07-12 16:51:39 +0100

    Setup project

---
+++ TaskListApp/Controller/TaskListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TaskListViewController.swift
+//  TaskListApp
+//
+//  Created by Sergey Zakurakin on 12/07/2024.
+//
+
+import UIKit
+
+final class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
