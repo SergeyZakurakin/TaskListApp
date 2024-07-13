a9a3f49381460972c2a956a715515329ea80f00d
SergeyZakurakin <pricezak@gmail.com>
2024-07-13 16:42:52 +0100

    setup UI

--- TaskListApp/Controller/TaskListViewController.swift
+++ TaskListApp/Controller/TaskListViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 final class TaskListViewController: UITableViewController {
+    
+    //MARK: - UI
+    
 
+    
+    
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupNavigationBar()
+        view.backgroundColor = .systemBackground
+        
     }
+    
+    @objc private func addNewTask() {
+        let newTaskVC = NewTaskViewController()
+        present(newTaskVC, animated: true)
+        
+        
+        // метод пирехода экрана
+        
+        
+    }
+    
+    
+}
+    //MARK: - Setup UI
+    
 
-
+extension TaskListViewController {
+    
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // navigation bar Appearance
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        
+        navBarAppearance.backgroundColor = .milkBlue
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
+        
+        // add button to navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    
+    
 }
 
