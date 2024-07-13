2012c8e7308d05e5a41e5a59320fb3d339e973f4
SergeyZakurakin <pricezak@gmail.com>
2024-07-13 22:00:58 +0100

    finish task with Alert Controller

--- TaskListApp/Controller/TaskListViewController.swift
+++ TaskListApp/Controller/TaskListViewController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 
-protocol NewTaskViewControllerDelegate: AnyObject {
-    func reloadData()
-}
+
 
 
 final class TaskListViewController: UITableViewController {
@@ -34,10 +32,8 @@ final class TaskListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        // метод пирехода экрана
-        let newTaskVC = NewTaskViewController()
-        newTaskVC.delegate = self
-        present(newTaskVC, animated: true)
+        showAlert(withTitle: "New Task", andMassage: "What do you want to do")
+
         
     }
     
@@ -50,6 +46,33 @@ final class TaskListViewController: UITableViewController {
         } catch {
             print(error)
         }
+    }
+    
+    private func showAlert(withTitle title: String, andMassage massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save Task", style: .default) { [unowned self] _ in
+            guard let taskName = alert.textFields?.first?.text, !taskName.isEmpty else { return }
+            save(taskName)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let task = ToDoTask(context: appDelegate.persistentContainer.viewContext)
+        task.title = taskName
+        taskList.append(task)
+        
+        let indexPath = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        appDelegate.saveContext()
     }
     
 }
@@ -118,10 +141,3 @@ extension TaskListViewController {
     
 }
 
-extension TaskListViewController: NewTaskViewControllerDelegate {
-    func reloadData() {
-        fetchData()
-        tableView.reloadData()
-    }
-    
-}
